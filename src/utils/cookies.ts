310c287d625592c04c4b32de3a4d4c265710311f
ambrose-kibet <ambrosekibet36@gmail.com>
2023-08-28 23:30:30 +0300

    craate  send cookies functionality

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,3 +1,58 @@
-import { Response } from 'express';
-
-export const attachResToCookie = (token: string, res: Response) => {};
+import { Response, request } from 'express';
+import Token from '../Models/tokenModel';
+import crypto from 'crypto';
+import mongoose from 'mongoose';
+import { createJwt } from './jwt';
+import UnauthorizedError from '../Errors/UnauthorizedError';
+export const createrefreshToken = async (
+  req: any,
+  userId: mongoose.Types.ObjectId
+) => {
+  let refreshToken: string = '';
+  // check if reresh Token exists
+  const token = await Token.findOne({ user: userId });
+  if (token) {
+    if (!token.isValid) throw new UnauthorizedError('Invalid credentials');
+    refreshToken = token.refreshToken;
+  } else {
+    refreshToken = crypto.randomBytes(40).toString('hex');
+    const userAgent = req.headers['user-agent'];
+    const user = userId;
+    const ip = req.ip;
+    const tempToken = { refreshToken, userAgent, ip, user };
+    await Token.create(tempToken);
+  }
+  return refreshToken;
+};
+type payloadType = {
+  userId: mongoose.Types.ObjectId;
+  fullName?: string;
+  role?: string;
+  refreshToken: string;
+};
+export const attachResToCookie = (payload: payloadType, res: Response) => {
+  const refreshTokenJwt = createJwt({
+    userId: payload.userId,
+    refreshToken: payload.refreshToken,
+  });
+  const accessTokenJwt = createJwt({
+    userId: payload.userId,
+    fullName: payload.fullName,
+  });
+  const oneHour = 1000 * 60 * 60;
+  const twoWeeks = oneHour * 24 * 14;
+  res.cookie('accessToken', accessTokenJwt, {
+    httpOnly: true,
+    // secure: true,
+    sameSite: 'lax',
+    signed: true,
+    maxAge: oneHour,
+  });
+  res.cookie('refreshToken', refreshTokenJwt, {
+    httpOnly: true,
+    // secure: true,
+    sameSite: 'lax',
+    signed: true,
+    expires: new Date(Date.now() + twoWeeks),
+  });
+};
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,21 +1,18 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import mongoose from 'mongoose';
 
-type mongoId = {
-  _id: mongoose.Types.ObjectId;
-  fullName: string;
+export type mongoId = {
+  userId: mongoose.Types.ObjectId;
+  fullName?: string;
+  role?: string;
+  refreshToken?: string;
 };
-export const createToken = (user: mongoId): string => {
-  const { _id, fullName } = user;
-
-  const token = jwt.sign(
-    { userId: _id, user, fullName },
-    process.env.JWT_SECRET!
-  );
+export const createJwt = (user: mongoId): string => {
+  const token = jwt.sign(user, process.env.JWT_SECRET!);
   return token;
 };
 
-export const verifyToken = (token: string) => {
-  const payload = jwt.verify(token, process.env.JWT_SECRET!);
+export const verifyJwt = (token: string) => {
+  const payload = jwt.verify(token, process.env.JWT_SECRET!) as mongoId;
   return payload;
 };
