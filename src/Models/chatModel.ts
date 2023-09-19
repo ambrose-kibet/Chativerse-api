ad13fb97ab0d13ab50f62d7e538aea528e4a4b9a
ambrose-kibet <ambrosekibet36@gmail.com>
2023-09-19 13:42:35 +0300

    add unread messages field

--- src/Models/chatModel.ts
+++ src/Models/chatModel.ts
@@ -1,22 +1,34 @@
-import mongoose, {
-  Schema,
-  model,
-  Model,
-  Types,
-  InferSchemaType,
-} from 'mongoose';
+import mongoose from 'mongoose';
+import { Schema, model, Model, Types, InferSchemaType } from 'mongoose';
 
 const chatSchema = new Schema(
   {
-    members: {
-      type: [{ type: Types.ObjectId, ref: 'User' }],
+    member1: {
+      type: Types.ObjectId,
       required: true,
+      ref: 'User',
+    },
+    member2: {
+      type: Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+
+    unreadMessages: {
+      type: Map,
+      of: Number, // Map of user IDs to unread message counts
+      default: {},
     },
   },
   { timestamps: true }
 );
+
+// Define a custom validator to ensure unique chat with the same members
+chatSchema.index({ member1: 1, member2: 1 }, { unique: true });
+
 export type IChat = InferSchemaType<typeof chatSchema>;
-const Chat: Model<IChat> = model<IChat>('Chat', chatSchema);
+const Chat = model<IChat>('Chat', chatSchema);
+
 chatSchema.post('findOneAndDelete', async function (doc) {
   if (doc) {
     const Message = mongoose.connection.model('Message');
