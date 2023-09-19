e5593f99bcf3eb8cc36e3a0e40c587e16211a9c7
ambrose-kibet <ambrosekibet36@gmail.com>
2023-09-19 13:45:27 +0300

    add increment unread

--- src/Controllers/messageController.ts
+++ src/Controllers/messageController.ts
@@ -2,15 +2,11 @@ import BadRequestError from '../Errors/BadRequestError';
 import Message from '../Models/messageModel';
 import { Request, Response } from 'express';
 import StatusCodes from 'http-status-codes';
+import Chat from '../Models/chatModel';
 const { CREATED, OK } = StatusCodes;
 const createMesage = async (req: any, res: Response) => {
   const { chatId, message, imageUrl, recipient } = req.body;
-  if (
-    !chatId ||
-    !recipient ||
-    (!message && !imageUrl) ||
-    (!imageUrl && !message)
-  )
+  if (!chatId || !recipient || (!message && !imageUrl))
     throw new BadRequestError('please provide chatId, recipient and message');
   const newMessage = await Message.create({
     chat: chatId,
@@ -19,6 +15,13 @@ const createMesage = async (req: any, res: Response) => {
     text: message,
     imageUrl,
   });
+  const chat = await Chat.findById(chatId);
+  if (!chat) throw new BadRequestError('chat not found');
+  chat.unreadMessages.set(
+    recipient,
+    (chat.unreadMessages.get(recipient) || 0) + 1
+  );
+  await chat.save();
   res.status(CREATED).json({ message: newMessage });
 };
 
@@ -26,9 +29,11 @@ const getMessages = async (req: any, res: Response) => {
   const { chatId } = req.params;
   const messages = await Message.find({
     chat: chatId,
-    $or: [{ sender: req.user.userId }, { recipient: req.user.userId }],
-  });
-
+  }).sort('createdAt');
+  const chat = await Chat.findById(chatId);
+  if (!chat) throw new BadRequestError('chat not found');
+  chat.unreadMessages.set(req.user.userId, 0);
+  await chat.save();
   res.status(OK).json({ messages });
 };
 export { createMesage, getMessages };
