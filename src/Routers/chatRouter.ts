fb8b003384d195ed7137244013cf6eb02b099fc9
ambrose-kibet <ambrosekibet36@gmail.com>
2023-09-19 13:41:44 +0300

    add mark as read handler

--- src/Routers/chatRouter.ts
+++ src/Routers/chatRouter.ts
@@ -4,9 +4,10 @@ import {
   getAllMyChats,
   getSingleChat,
   createChat,
+  markMessagesAsRead,
 } from '../Controllers/chatController';
 
 router.route('/').post(createChat).get(getAllMyChats);
-router.route('/:chatId').get(getSingleChat);
+router.route('/:chatId').get(getSingleChat).patch(markMessagesAsRead);
 
 export default router;
